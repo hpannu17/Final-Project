204f46ed73da78b37a198efbde6b0d8fbec2cd09
hpannu17 <54556224+hpannu17@users.noreply.github.com>
2019-12-04 16:27:46 -0500

    title done

---
+++ script/Title.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var InfoPopupMenu
+func _ready():
+	InfoPopupMenu  = get_node('InfoPopupMenu')
+	pass
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene('res://Scenes/World.tscn')
+	pass 
+
+
+func _on_InfoButton_pressed():
+	InfoPopupMenu.show()
+	pass # Replace with function body.
+
+
+func _on_CloseButton_pressed():
+	InfoPopupMenu.hide()
+	pass
